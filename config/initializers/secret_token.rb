aee15fbe8ee4a80fcc12912a1a96d5bf13b67ad9
lookmee <lookmee@newton.localdomain>
2014-02-25 18:06:33 +0700

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cmscart::Application.config.secret_key_base = '3e69572d2a54d73ae15dad9c805195a4002871aeab55a5e639cb3e0d929e3c1fa57c4cc00ae5abae3fc965fb0ca18c70c629b2abf195cdf25e73c48982e941ac'
